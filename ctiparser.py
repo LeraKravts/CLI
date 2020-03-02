ee6ed46034b3ade2d498963a16fe6307ff8128c7
ValeriaKravtsova <lera.gor@inbox.ru>
2020-03-02 17:04:10 +0300

    drop html parser

--- ctiparser.py
+++ ctiparser.py
@@ -1,9 +1,17 @@
+from lxml import html
+from html import text
 import argparse
 
 
-def cti_parser():
+def cli_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--xpath', default='//*', help='a part of html page')
     args = parser.parse_args()
-    return args.xpath
+    path = args.xpath
 
+    source_code = html.fromstring(text)
+    tree = source_code.xpath(path)
+    return print(tree[0].text_content())
+
+
+cli_parser()
