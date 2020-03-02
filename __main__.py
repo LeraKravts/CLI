cecc26f8d6b03f1f5d178fefaeb790cacd436eec
ValeriaKravtsova <lera.gor@inbox.ru>
2020-03-02 16:24:46 +0300

    add main

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from html_parser import html_parsing
+
+if __name__ == '__main__':
+    html_parsing()
+
--- html_parser.py
+++ html_parser.py
@@ -4,8 +4,11 @@ from html_doc import text
 from ctiparser import cti_parser
 
 
-path = str.encode(cti_parser())
-response = text
-source_code = html.fromstring(text)
-tree = source_code.xpath(path)
-print(tree[0].text_content())
+def html_parsing():
+
+    path = str.encode(cti_parser())
+    response = text
+    source_code = html.fromstring(text)
+    tree = source_code.xpath(path)
+    return print(tree[0].text_content())
+
