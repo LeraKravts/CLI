0f633c5004bc2cfade0ebb126a0087665edcfa12
ValeriaKravtsova <lera.gor@inbox.ru>
2020-03-02 16:17:44 +0300

    add html_parser

--- ctiparser.py
+++ ctiparser.py
@@ -5,8 +5,5 @@ def cti_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--xpath', default='//*', help='a part of html page')
     args = parser.parse_args()
-    return args
-
-
-print(cti_parser())
+    return args.xpath
 
--- html_parser.py
+++ html_parser.py
@@ -1,9 +1,11 @@
-import lxml
-from lxml import html
-from lxml import etree
-from html import text
 import requests
+from lxml import html
+from html_doc import text
 from ctiparser import cti_parser
 
 
-
+path = str.encode(cti_parser())
+response = text
+source_code = html.fromstring(text)
+tree = source_code.xpath(path)
+print(tree[0].text_content())
