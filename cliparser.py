5420daa5c803ed5df357e89df0a383d213a03d16
LeraKravts <60606888+LeraKravts@users.noreply.github.com>
2020-03-03 14:00:07 +0300

    Update cliparser.py
    add try/except

--- cliparser.py
+++ cliparser.py
@@ -7,8 +7,11 @@ def cli_parser(document: str):
     parser = argparse.ArgumentParser()
     parser.add_argument('--xpath', default='//*', help='a part of html page')
     args = parser.parse_args()
-   
-    source_code = html.fromstring(document)
-    tree = source_code.xpath(args.xpath)
     
-    return tree[0].text_content()
+    try:
+        source_code = html.fromstring(document)
+        tree = source_code.xpath(args.xpath)
+        return tree[0].text_content()
+    except IndexError:
+        print('IndexError')
+
